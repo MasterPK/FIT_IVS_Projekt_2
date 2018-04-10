4b372f84136e82586c76cc9efc58bf166f4c4081
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2018-04-10 10:13:58 +0200

    vytvoreni knihovny math

---
+++ IVS/IVS/math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+	class math
+	{
+		public double Soucet(double a, double b)
+		{
+			return a + b;
+		}
+	}
+}
