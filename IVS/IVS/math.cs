8aae369a144f152725f73358c970da46feef3d47
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2018-04-10 16:32:56 +0200

    funkce
    Test_int
    Faktorial
    Umocnit

--- IVS/IVS/math.cs
+++ IVS/IVS/math.cs
@@ -9,6 +9,17 @@ namespace MathLibrary
 	class math
 	{
 		/// <summary>
+		/// Testuje jestli zadane cislo je cele
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns>Vraci True pokud je cislo cele, jinak false</returns>
+		private static bool Test_Int(double a)
+		{
+				return true;
+			else
+				return false;
+		}
+		/// <summary>
 		/// Soucet dvou cisel na vstupu
 		/// </summary>
 		/// <param name="a"></param>
@@ -46,12 +57,48 @@ namespace MathLibrary
 		/// <exception cref="DivideByZeroException">Nastavi pri deleni nulou</exception>
 		/// <returns>Deleni vstupu</returns>
 		public static double Podil(double a, double b)
-        {
+		{
 			if (b == 0) {
 				throw new DivideByZeroException();
 			}
 			return a / b;
 		}
-		
+		/// <summary>
+		/// Vypocita faktorial ze zadaneho cisla
+		/// </summary>
+		/// <param name="a"></param>
+		/// <exception cref="ArgumentException">Pokud vstup neni prirozene cislo</exception>
+		/// <returns>Faktorial vstupu</returns>
+		public static int Faktorial(double a)
+		{
+			if (Test_Int(a) == false && a >= 0) {
+				throw new ArgumentException();
+			}
+			if (a == 0 || a == 1) {
+				return 1;
+			}
+			int f = 1;
+			for (int i = 1; i <= a; i++) {
+				f *= i;
+			}
+			return f;
+		}
+		public static double Umocnit(double x, double n)
+		{
+			if (Test_Int(n) == false && n >= 0) {
+				throw new ArgumentException();
+			}
+			if (n == 0) {
+				return 1;
+			}
+			double vysledek=x;
+			for (int i = 0; i < n-1; i++) {
+				vysledek = math.Nasob(vysledek,x);
+			}
+			return vysledek;
+
+
+
+		}
 	}
 }
