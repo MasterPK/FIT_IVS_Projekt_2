5bdc59122f1bda872a7c9a84b64a78cf03ddc8a3
klobco <xklobu03@stud.fit.vutbr.cz>
2018-04-11 12:54:36 +0200

    komentáre

--- IVS/IVS/Form1.cs
+++ IVS/IVS/Form1.cs
@@ -28,96 +28,191 @@ namespace IVS
 
         }
 
+        /// <summary>
+        /// Vloženie '0' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button4_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "0";
         }
 
+        /// <summary>
+        /// Vloženie '+' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button15_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "+";
         }
 
+        /// <summary>
+        /// vloženie '(' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button20_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "(";
         }
 
+        /// <summary>
+        /// Vloženie '1' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "1";
         }
 
+        /// <summary>
+        /// Vloženie '2' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "2";
         }
 
+        /// <summary>
+        /// Vloženie '3' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "3";
         }
 
+        /// <summary>
+        /// Vloženie '4' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button5_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "4";
         }
 
+        /// <summary>
+        /// Vloženie '5' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button6_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "5";
         }
 
+        /// <summary>
+        /// Vloženie '6' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button7_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "6";
         }
 
+        /// <summary>
+        /// Vloženie '7' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button8_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "7";
         }
 
+        /// <summary>
+        /// Vloženie '8' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button9_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "8";
         }
 
+        /// <summary>
+        /// Vloženie '9' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button10_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "9";
         }
 
+        /// <summary>
+        /// Vloženie ')' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button21_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + ")";
         }
 
+        /// <summary>
+        /// Vyčistenie textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button22_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
         }
 
+        /// <summary>
+        /// Vloženie ',' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button19_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + ",";
         }
 
+        /// <summary>
+        /// Vloženie '-' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button16_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "-";
         }
 
+        /// <summary>
+        /// Vloženie 'x' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button17_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "x";
         }
 
+        /// <summary>
+        /// Vloženie '/' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button18_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "/";
         }
 
+        /// <summary>
+        /// Vloženie '^' do textového pola
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button12_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "^";
